de35167da4b959163948970bcf9f350b8131925c
macbookpro <macbookpro@10.6.75.239>
2019-10-04 18:06:29 +0100

    Added all test constructors and main test for size. Put back in necessary code

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -4,9 +4,18 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.NoSuchElementException;
 
+
+
+
+
 public class LCA<Key extends Comparable<Key>, Value> 
 {
   
+	 private Node root;             // root of BST
+	    private String stringOutput = "";
+	    private String multiLineOutput = "";
+	    private String prefix = "";
+	    
     private class Node 
     {
         private Key key;           // sorted by key
@@ -21,6 +30,134 @@ public class LCA<Key extends Comparable<Key>, Value>
             this.val = val;
             this.N = N;
         }
+    }
+    
+    public int size() 
+    { 
+    	return size(root); 
+    }
+    
+    private int size(Node x) 
+    {
+        if (x == null) 
+        	return 0;
+        
+        else 
+        	return x.N;  
+    }
+    
+    public void put(Key key, Value val) 
+    {
+        if (val == null) 
+        { 
+        	delete(key); 
+        	return; 
+        }
+        root = put(root, key, val);
+    }
+    
+    public void delete(Key key)
+    { 
+    	root = delete(root, key); 
+    }
+    
+    private Node delete(Node x, Key key)
+    {
+    	if (x == null) 
+    		return null;
+    	
+    	int compare = key.compareTo(x.key);
+    	
+    	if (compare < 0) x.left = delete(x.left, key);
+    	
+    	else if (compare > 0) x.right = delete(x.right, key);
+    	
+    	else
+    	{
+    		if (x.right == null) 
+    			return x.left;
+    		
+    		if (x.left == null) 
+    			return x.right;
+    		
+    		Node t = x;
+    		x = max(t.left); 
+    		x.left = deleteMax(t.left);
+    		x.right = t.right;
+    	}
+    	
+    	x.N = size(x.left) + size(x.right) + 1;
+    	return x;
+    }
+
+    private Node put(Node x, Key keyToPlaceValIn, Value newValue) 
+    {
+        if (x == null) 
+        	return new Node(keyToPlaceValIn, newValue, 1);
+        
+        int cmp = keyToPlaceValIn.compareTo(x.key);
+        
+        if (cmp < 0) 
+        	x.left  = put(x.left,  keyToPlaceValIn, newValue);
+        
+        else if (cmp > 0) 
+        	x.right = put(x.right, keyToPlaceValIn, newValue);
+        
+        else             
+        	x.val   = newValue;
+        
+        x.N = 1 + size(x.left) + size(x.right);
+        
+        return x;
+    }
+    
+    private Node deleteMax(Node x)
+    {
+    	if (x.right == null) 
+    		return x.left;
+    	
+    	x.right = deleteMax(x.right);
+    	
+    	x.N = size(x.right) + size(x.left) + 1;
+    	
+    	return x;
+    }
+    
+    private Node max(Node x)
+    {
+    	if (x.right == null) 
+    		return x;
+    	
+    	return max(x.right);
+    }
+    
+    public String printKeysInOrder()
+    {
+    	stringOutput = "";
+    	
+    	if (this.root == null) 
+    		return "()";
+    	
+    	else
+    		return "(" + printKeysInOrder(this.root) + ")";
+    }
+    
+    private String printKeysInOrder(Node x)
+    {
+    	if (x == null) 
+    		return "";
+    	
+    	stringOutput += "(";
+    	printKeysInOrder(x.left);
+    	stringOutput += ")";
+    	
+    	stringOutput += (x.key);
+    	
+    	stringOutput += "(";
+    	printKeysInOrder(x.right);
+    	stringOutput += ")";
+    	
+    	return stringOutput;
     }
 
     
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -5,8 +6,39 @@ import org.junit.jupiter.api.Test;
 class LCATest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testLCA() {
+		
 	}
 
+	
+	@Test
+	public void testSize() {
+		LCA<Integer, Integer> lca = new LCA<Integer, Integer>();
+		assertEquals(0, lca.size());
+		
+		lca.put(1, 1); 
+		assertEquals(1, lca.size());
+		
+		lca.put(2, 2);     
+		lca.put(3, 3);       
+		lca.put(4, 4);      
+		lca.put(5, 5);      
+		lca.put(6, 6);       
+		lca.put(7, 7);       
+		lca.put(8, 8); 
+		assertEquals(8, lca.size());
+	}
+	// Got this test from the same project I used from last year
+	
+	@Test
+	public void testPut() {
+		
+	}
+	
+	@Test
+	public void testDelete() {
+		
+	}
+	
+	
 }
