735f58c5d2d54c07affdbd3bd7848284650f2287
macbookpro <macbookpro@10.6.92.100>
2019-10-10 14:02:58 +0100

    Started DAG class with methods

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
 
 public class DAG {
 
+		private int V;
+		private int E;
+		private ArrayList<Integer>[] adj;
+	
+	
+		public DAG(int V) {
+			
+		}
+		
+		public int V() {
+			return V;
+			//returns number of vertices
+		}
+		
+		public int E() {
+			return E;
+			//returns number of edges
+		}
+		
+		public void addEdge(int v, int w) {
+			
+		}
+		
+		public Iterable<Integer> adj(int v){
+			return null;
+			//finds vertics that are adjacent
+		}
+		
+		public DAG reverse() {
+			return null;
+			///returns DAG in reverse order
+		}
+		
+	
+	
 }
