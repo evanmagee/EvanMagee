6046f348792897309b2a488f2f77d59efb8f90b8
macbookpro <macbookpro@localhost>
2019-10-15 11:21:16 +0100

    Added test for add edge method

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -5,10 +6,21 @@ import org.junit.jupiter.api.Test;
 class DAGTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 	
-	//test
+	public void testAddEdge() {
+		DAG tester = new DAG (6);
+		tester.addEdge(2,3);
+		assertEquals("Number of edges should be 1", 1, tester.E());
+		
+		tester.addEdge(3,4);
+		assertEquals("Number of edges should be 2",2,tester.E());
+		
+		tester.addEdge(4,5);
+		assertEquals("Number of edges should be 3",3,tester.E());
+		
+		tester.addEdge(-2,4);
+		//this shouldnt add an edge
+		assertEquals("Number of edges should be 3",3,tester.E());
+	}
 
 }
